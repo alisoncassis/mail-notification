97c19db438796a3ccfe70235d0e7946316fc3ff3
alisoncassisn <alisoncassisn@gmail.com>
2018-02-16 08:31:01 -0200

    config e procfile

---
+++ config/express.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const mailService = require('../services/mail');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/notificate', (req, res) => {
+  mailService.send({ body: req.body });
+  res.status(200).send();
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const mailService = require('./services/mail');
-const bodyParser = require('body-parser');
-const path = require('path');
-const app = express();
+const app = require('./config/express');
 
 app.listen(process.env.PORT, () => console.log(`server running at ${process.env.PORT}`));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.post('/notificate', (req, res) => {
-  mailService.send({ body: req.body });
-  res.status(200).send();
-});
