5316319ebb202b62521da18c8baec4a795dde409
alisoncassisn <alisoncassisn@gmail.com>
2018-02-17 13:40:19 -0200

    privacy policy

--- config/express.js
+++ config/express.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const express = require('express');
 const mailService = require('../services/mail');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/policy', (req, res) => res.sendFile(path.join(__dirname, '../static/privacy_policy.html')));
 
 app.post('/notificate', (req, res) => {
   mailService.send({ body: req.body });
