66a04face4d181858a9c735275daedd450f81c27
alisoncassisn <alisoncassisn@gmail.com>
2018-02-16 22:13:48 -0200

    cors

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mailService = require('../services/mail');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
