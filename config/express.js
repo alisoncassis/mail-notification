0a2d23dc71e01aadf6ea8cd6c1a1254da2e0b6f6
alisoncassisn <alisoncassisn@gmail.com>
2018-10-28 13:56:54 -0300

    privacy policy and terms of use

--- config/express.js
+++ config/express.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/policy', (req, res) => res.sendFile(path.join(__dirname, '../static/privacy_policy.html')));
+app.get('/policy', (req, res) => res.sendFile(path.join(__dirname, '../static/app/privacy_policy.html')));
+app.get('/web/politica-de-privacidade', (req, res) => res.sendFile(path.join(__dirname, '../static/web/privacy_policy.html')));
+app.get('/web/termos-de-uso', (req, res) => res.sendFile(path.join(__dirname, '../static/web/terms-of-use.html')));
 
 app.post('/autonomous-user', (req, res) => {
     mailService.send({ body: req.body, type: 'newUser' });
